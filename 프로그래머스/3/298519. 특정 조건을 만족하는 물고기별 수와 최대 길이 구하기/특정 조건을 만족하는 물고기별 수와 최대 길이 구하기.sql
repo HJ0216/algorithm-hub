cae6a0251ef222101a9c486f033075489577d008
Hyunji <6120hj@gmail.com>
2025-09-11 22:32:57 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -0,0 +1,13 @@
+select
+    count(id) as FISH_COUNT
+    , max(ifnull(length, 10)) as MAX_LENGTH
+    , fish_type as FISH_TYPE
+from
+    fish_info
+group by
+    fish_type
+having
+    avg(ifnull(length, 10)) >= 33
+order by
+    fish_type
+;
