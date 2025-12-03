796ac0892b539945290b0a1f429060559b9dcb0b
Hyunji <6120hj@gmail.com>
2025-12-03 21:12:01 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        int originalX = x;
+        int sum = 0;
+        while(x>0){
+            sum += (x % 10);
+            x /= 10;
+        }
+        
+        if(originalX % sum ==0){
+            return true;
+        }
+        
+        return false;
+    }
+}
