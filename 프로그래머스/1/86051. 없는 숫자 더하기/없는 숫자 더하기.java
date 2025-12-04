2b74b4240f10860619f40b807d489677e85b5873
Hyunji <6120hj@gmail.com>
2025-12-04 19:51:38 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int[] numbers) {
         int result = 45;
-        for(int i=0; i<numbers.length; i++){
-            result -= numbers[i];
+        for(int num : numbers){
+            result -= num;
         }
         
         return result;
