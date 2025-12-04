85809fa4fc745fa88a8dceacd04fc063ec4e22b4
Hyunji <6120hj@gmail.com>
2025-12-04 19:45:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,12 +1,9 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int result = 0;
+        
         for(int i=0; i<signs.length; i++){
-            if(signs[i]){
-                result += absolutes[i];
-            } else {
-                result -= absolutes[i];
-            }
+            result += signs[i] ? absolutes[i] : -absolutes[i];
         }
         
         return result;
