0c3a7523d5d6a678a62775cd45bf682a2c5aa56a
Hyunji <6120hj@gmail.com>
2025-12-04 20:03:37 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.73 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,16 +1,6 @@
 class Solution {
     public String solution(String phone_number) {
-        char[] chars = phone_number.toCharArray();
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i=chars.length-1; i >= 0; i--){
-            if(i <= chars.length-5){
-                sb.append("*");
-            } else{
-                sb.append(chars[i]);
-            }
-        }
-        
-        return sb.reverse().toString();
+        int len = phone_number.length();
+        return "*".repeat(len-4) + phone_number.substring(len - 4);
     }
 }
