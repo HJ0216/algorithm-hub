63dbe46513f7c4bf1260994c42dd63a2e93c71ca
Hyunji <6120hj@gmail.com>
2025-12-05 06:58:09 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.56 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -8,18 +8,11 @@ class Solution {
             if(i<min) min = i;
         }
         
-        boolean isMinRemoved = false;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == min){
-                isMinRemoved = true;
-                continue;
-            }
-            
-            if(isMinRemoved){
-                answer[i-1] = arr[i];
-            } else {
-                answer[i] = arr[i];
-            }
+        int index = 0;
+        for(int i : arr){
+            if(i != min){
+                answer[index++] = i;
+            }            
         }
         
         return answer;
