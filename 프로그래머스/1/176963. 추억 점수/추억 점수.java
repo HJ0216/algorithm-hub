663ddf84c5dbb576d31e4f67d23285bd189d9c89
Hyunji <6120hj@gmail.com>
2025-12-06 08:29:15 +0900

    [level 1] Title: 추억 점수, Time: 2.36 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,25 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        
-        int[] missings = new int[photo.length];
-        
-        int index = 0;
-        for(String[] people : photo){            
-            for(String person : people){
-                for(int i=0; i<name.length; i++){
-                    if(i>=yearning.length){
-                        break;
-                    }
-                    
-                    if(person.equals(name[i])){
-                        missings[index] += yearning[i];
-                    }
-                }
-            }
-            
-            index++;
+        Map<String, Integer> scores = new HashMap<>();
+        for(int i=0; i<name.length; i++){
+            scores.put(name[i], yearning[i]);
         }
         
-        return missings;
+        int[] answer = new int[photo.length];
+        for(int i=0; i<photo.length; i++){
+            int sum =0;
+            for(String person : photo[i]){
+                sum += scores.getOrDefault(person, 0);
+            }
+            answer[i] = sum;
+        }
+
+        return answer;
     }
 }
