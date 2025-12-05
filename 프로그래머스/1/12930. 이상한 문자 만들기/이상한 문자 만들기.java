e3765c22cc91a947d9d8c8e9251b3badf7c98bfc
Hyunji <6120hj@gmail.com>
2025-12-06 06:04:32 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.10 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,22 +1,19 @@
 class Solution {    
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
-        
-        boolean isUpperCase = true;
-        
+
+        int index = 0;
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
             if(c == ' '){
-                sb.append(String.valueOf(c));
-                isUpperCase = true;
+                sb.append(c);
+                index = 0;
             } else {
-                if(isUpperCase){
-                    sb.append(String.valueOf(Character.toUpperCase(c)));               
-                    isUpperCase = false;
-                } else {
-                    sb.append(String.valueOf(Character.toLowerCase(c)));               
-                    isUpperCase = true;
-                }
+                sb.append(index % 2 == 0 
+                  ? Character.toUpperCase(c) 
+                  : Character.toLowerCase(c));               
+                
+                index++;
             }
         }
         
