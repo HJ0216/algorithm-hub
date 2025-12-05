acdbdb352f64b5979042116523caeb9e0ce4fb6d
Hyunji <6120hj@gmail.com>
2025-12-06 07:21:54 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 8.30 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,22 +3,10 @@ class Solution {
         StringBuilder sb = new StringBuilder();
 
         for(int i=1; i<food.length; i++){
-            if(food[i] < 2){
-                continue;
-            }
-            
-            if(food[i] %2 !=0) {
-                food[i] -= 1;
-            }
-            
-            sb.append(String.valueOf(i).repeat(food[i]/2));
+            int count = food[i] / 2;
+            sb.append(String.valueOf(i).repeat(count));
         }
         
-        StringBuilder copy = new StringBuilder(sb);
-
-        sb.append("0");
-        sb.append(copy.reverse());
-        
-        return sb.toString();
+        return sb.toString() + "0" + sb.reverse().toString();
     }
 }
