528245c45e789e8e5bbf1ef29f92e74c93e5b429
Hyunji <6120hj@gmail.com>
2025-12-06 13:10:26 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.58 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -4,27 +4,15 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         
-        // 3갸만 정렬
-        // 크면 넣어주고 다시 정렬 후 min
-        
-        int [] trio = new int[k];        
-        trio[0] = score[0];
-        answer[0] = trio[0];
-        for(int i=1; i<score.length; i++){
-            if(i < k){
-                trio[i] = score[i];
-                answer[i] = Math.min(answer[i-1], trio[i]);
-            } else {
-                if(score[i] > answer[i-1]){
-                    Arrays.sort(trio);
-                    trio[0] = score[i];
-                    Arrays.sort(trio);
-                    answer[i] = trio[0];
-                } else {
-                    answer[i] = answer[i-1];                    
-                }
-            }             
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < score.length; i++) {
+            queue.offer(score[i]);
+            if(queue.size() > k){
+                queue.poll();
+            }
             
+            answer[i] = queue.peek();
         }
 
         return answer;
